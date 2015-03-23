d1aae3c7e33c185c2e43fe65ca05b0cfebecbeb8
Yunhe (John) Wang <yhjw88@gmail.com>
2015-03-23 09:46:53 -0400

    Deleted ^M from line endings

--- query.py
+++ query.py
@@ -224,7 +224,8 @@ def freebaseTopic(mid, key):
             break
     if not success:
         return False
-
+    
+    # TODO: Formatting
     # TODO: Get infobox properties
     if entityTypes[PERSON]:
         print "Person"
