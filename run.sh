b97ee2c8cbfa5fafc0e7274e39a341230065abeb
eshamaharishi <em2852@columbia.edu>
2015-03-27 17:00:55 -0400

    run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#echo $1
+#python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Harry Potter?' -t question
+
+#
+ python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Bill Gates' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Robert Downey Jr.' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Jackson' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NFL' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NBA' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NY Knicks' -t infobox
+# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Miami heat' -t infobox
+#
