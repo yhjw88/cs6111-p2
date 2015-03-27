b430f20120a5eb90cb64b954694b5ad190b334ae
eshamaharishi <em2852@columbia.edu>
2015-03-27 17:21:17 -0400

    run.sh with all required output for submission

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
-#echo $1
-#python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Harry Potter?' -t question
+# infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Bill Gates' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Robert Downey Jr.' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Jackson' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NFL' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NBA' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NY Knicks' -t infobox
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Miami Heat' -t infobox
 
-#
- python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Bill Gates' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Robert Downey Jr.' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Jackson' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NFL' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NBA' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'NY Knicks' -t infobox
-# python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Miami heat' -t infobox
-#
+# question
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Google?' -t question
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Lord of the Rings?' -t question
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Microsoft?' -t question
+python query.py -key AIzaSyANUlKvip8QlgqzNAG3EpJi_SzH3ne0N9o -q 'Who created Romeo and Juliet?' -t question
